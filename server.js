cc5323077c789215f78ca357b2120e9be83fea9c
Piyush Santwani <piyush.santwani@justride.in>
2016-06-19 07:32:58 +0530

    writeKey api

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 var portastic = require('portastic');
-var express			= require('express');
-var app				= express();
-var http			= require('http').Server(app);
-var port			= process.env.PORT || 3000;
+var express                     = require('express');
+var app                         = express();
+var http                        = require('http').Server(app);
+var port                        = process.env.PORT || 3000;
+var fs                          = require('fs-extra');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.get('/getFreePort', function(req,res){
-	portastic.find({
+        portastic.find({
     min: 10000,
     max: 12000
   })
   .then(function(ports){
     res.send(ports[0].toString());
   });
+});
+
+app.post('/writeKey', function(req,res){
+console.log(req.body);
+fs.readFile('/home/master/.ssh/authorized_keys', 'utf8', function(err,data){
+  var newKeySet = data + "\n" + req.body.key;
+fs.writeFile('/home/master/.ssh/authorized_keys', newKeySet, function (err) {
+      if (err) { throw err; }
+      console.log('Success');
+        res.send('Success');
+    });
+
+});
 });
 
 http.listen(port, function(){
